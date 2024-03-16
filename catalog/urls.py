437052a95a6d9f0ddf4451d5ff216aefb566331e
ganzolder <gnmsapr@gmail.com>
2024-03-16 21:30:15 +0700

    Error

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from catalog.views import index, contacts
+from catalog.apps import MainConfig
+from catalog.views import index, contacts, products_catalog, product_page, category_catalog
+
+
+app_name = MainConfig.name
 
 urlpatterns = [
     path('', index),
     path('contacts/', contacts, name='contacts'),
+    path('<int:pk>/products_catalog/', products_catalog, name='products_catalog'),
+    path('product_page/', product_page, name='product_page'),
+    path('category_catalog/', category_catalog, name='category_catalog'),
 
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from catalog.models import Category, Product
 
 # Create your views here.
 def index(request):
@@ -10,3 +11,28 @@ def contacts(request):
         email = request.POST.get('email')
         print(f'{name}, {email}')
     return render(request, 'main/contacts.html')
+
+def products_catalog(request, pk):
+
+    category_item = Category.objects.get(pk=pk)
+
+    context = {
+        'object_list': Product.objects.filter(category_id=pk),
+        'title' : f'Yoni products catalogue{category_item}'
+    }
+    return render(request, 'main/products_catalog.html', context)
+
+def category_catalog(request):
+    context = {
+        'object_list': Category.objects.all(),
+        'title' : 'Yoni category catalogue'
+    }
+    return render(request, 'main/category_catalog.html', context)
+
+
+def product_page(request):
+    context = {
+        'object_list': Product.objects.all(),
+        'title': 'Yoni product page'
+    }
+    return render(request, 'main/product_page.html', context)
